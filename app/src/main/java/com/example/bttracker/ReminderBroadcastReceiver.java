34e12cd2a09e21ab705cf8db892bf141ea6c64df
2020M017 <2020m017@kuas.ac.jp>
2020-06-10 21:03:28 +0900

    Homework 5.2: implemented OnClickListener for "SET REMINDER" button

--- app/src/main/java/com/example/bttracker/ReminderBroadcastReceiver.java
+++ app/src/main/java/com/example/bttracker/ReminderBroadcastReceiver.java
@@ -1,8 +1,11 @@
 package com.example.bttracker;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -11,15 +14,19 @@ public class ReminderBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // An Intent broadcast.
+        Intent notificationIntent = new Intent(context, LogActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        NotificationManager nm = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        Resources res = context.getResources();
         NotificationCompat.Builder builder = new
                 NotificationCompat.Builder(context, "BT_Tracker_Channel")
-                // this second argument has to match the Channel ID defined in MainActivity.java
                 .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setContentTitle("Notification from BT Tracker")
                 .setContentText("Please log your body temperature now")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT) ;
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // An Intent broadcast.
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         notificationManager.notify(200, builder.build());
     }
